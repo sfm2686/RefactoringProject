486988aa44df5e7579f6021f40002f2a5a7dcceb
Sultan Mira <Muro@Sultans-MacBook-Pro.local>
2016-07-30 17:44:11 -0400

    Replaced the project file I just made with one that IDE made. Because the IDE was not showing the project I manually made in the file tree

---
+++ RefactoringAlgo/src/Model/PinsetterObserver.java
@@ -0,0 +1,13 @@
+package Model;
+
+
+
+public interface PinsetterObserver {
+
+	/** recievePinsetterEvent()
+	 * 
+	 * defines the method for an object torecieve a pinsetter event
+	 */
+	public void receivePinsetterEvent(PinsetterEvent pe);
+};
+
